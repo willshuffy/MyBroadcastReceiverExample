e203b56c9de3c0254bc76341cba26678e50689ab
willshuffy <saputrawilly26@gmail.com>
2020-10-28 11:26:58 +0700

    SMS Receiver

--- app/src/main/java/com/willshuffyproject/mybroadcastreceiverexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/mybroadcastreceiverexample/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.willshuffyproject.mybroadcastreceiverexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object{
+        private const val SMS_REQUEST_CODE = 101
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_permission.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when {
+            v.id == R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == SMS_REQUEST_CODE){
+            when{
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/willshuffyproject/mybroadcastreceiverexample/SmsReceiverActivity.kt
@@ -0,0 +1,35 @@
+package com.willshuffyproject.mybroadcastreceiverexample
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sms_receiver.*
+
+class SmsReceiverActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val EXTRA_SMS_NO = "extra_sms_no"
+        const val EXTRA_SMS_MESSAGE = "extra_sms_message"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_receiver)
+
+        title = getString(R.string.incoming_message)
+
+        btn_close.setOnClickListener(this)
+
+        val senderNo = intent.getStringExtra(EXTRA_SMS_NO)
+        val senderMessage = intent.getStringExtra(EXTRA_SMS_MESSAGE)
+
+        tv_from.text = getString(R.string.from, senderNo)
+        tv_message.text = senderMessage
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_close) {
+            finish()
+        }
+    }
+}
